142f61835ed4bdb55b868916d194d990ee51d92d
mfitzasp <mfitzasp@gmail.com>
2025-02-01 10:58:53 +1100

    lcs configs

--- configs/lcs/wema_config.py
+++ configs/lcs/wema_config.py
@@ -109,8 +109,8 @@ wema_config = {
     'mpc_code':  'ZZ23',    #  This is made up for now.
     'time_offset':  11,   #  These two keys may be obsolete given the new TZ stuff
     'timezone': 'AEST',      #  This was meant to be coloquial Time zone abbreviation, alternate for "TX_data..."
-    'latitude': -37.70097222,     #  Decimal degrees, North is Positive
-    'longitude': 145.1918056,   #  Decimal degrees, West is negative
+    'latitude': -37.7827,     #  Decimal degrees, North is Positive
+    'longitude': 145.2702,   #  Decimal degrees, West is negative
     'elevation': 150,    #  meters above sea level
     'reference_ambient':  10,  #  Degrees Celsius.  Alternately 12 entries, one for every - mid month.
     'reference_pressure':  867.254,    #mbar   A rough guess 20200315
